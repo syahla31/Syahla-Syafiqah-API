06e1b32f4039fb4129d8fc851bc5c9617f7aeb00
syahla31 <syahlasyafiqah31@gmail.com>
2025-11-04 19:35:31 +0700

    updatee

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
